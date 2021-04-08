eb8f83e2c149bd1ca9ef153f293e1c9933de1ce3
Xom94o4ek <irek199707@gmail.com>
2021-04-08 11:32:38 +0300

    Добавьте файлы проекта.

---
+++ Chat/Program.cs
@@ -0,0 +1,43 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace Chat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "Client1";
+            var options = new ConfigurationOptions();
+            options.EndPoints.Add("localhost", 6379);
+            options.ClientName = name;
+
+            var redis = ConnectionMultiplexer.Connect(options);
+            var db = redis.GetDatabase();
+
+            const string key = "chat:list";
+            var command = "";
+            Console.WriteLine($"Вы подключены к чату как {name}!");
+            //db.StringSet(key, "Test");
+            //Console.WriteLine(db.StringGet(key));
+            db.ListRightPush(key, $"{name} подключился к чату!");
+            int chatCount = Convert.ToInt32(db.ListLength(key));
+            while (command != "/exit")
+            {
+                int chatRefresh = Convert.ToInt32(db.ListLength(key));
+                if(chatRefresh > chatCount)
+                {
+                    for(int i = chatCount; i <= chatRefresh; i++)
+                    {
+                        Console.WriteLine(db.ListGetByIndex(key,i));
+                    }
+                    chatCount = chatRefresh;
+                }
+                command = Console.ReadLine();
+                if(command != "/exit" && command != "") db.ListRightPush(key, $"{name}: {command}");
+                else if (command == "/exit") db.ListRightPush(key, $"{name} покинул чат!");
+            }
+            Console.WriteLine("Connection closed...");
+        }
+    }
+}
---
+++ Client2/Program.cs
@@ -0,0 +1,45 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "Client2";
+            var options = new ConfigurationOptions();
+            options.EndPoints.Add("localhost", 6379);
+            options.ClientName = name;
+
+            var redis = ConnectionMultiplexer.Connect(options);
+            var db = redis.GetDatabase();
+
+            const string key = "chat:list";
+            var command = "";
+            Console.WriteLine($"Вы подключены к чату как {name}!");
+            //db.StringSet(key, "Test");
+            //Console.WriteLine(db.StringGet(key));
+            db.ListRightPush(key, $"{name} подключился к чату!");
+            int chatCount = Convert.ToInt32(db.ListLength(key));
+            while (command != "/exit")
+            {
+                int chatRefresh = Convert.ToInt32(db.ListLength(key));
+                if (chatRefresh > chatCount)
+                {
+                    for (int i = chatCount; i <= chatRefresh; i++)
+                    {
+                        Console.WriteLine(db.ListGetByIndex(key, i));
+                    }
+                    chatCount = chatRefresh;
+                }
+                command = Console.ReadLine();
+                if (command != "/exit" && command != "") db.ListRightPush(key, $"{name}: {command}");
+                else if (command == "/exit") db.ListRightPush(key, $"{name} покинул чат!");
+            }
+            Console.WriteLine("Connection closed...");
+        }
+    }
+}
